da9162e4f39fd059f3491059ca07d64a16b86c9d
ricksilva2 <silvarico474@gmail.com>
2026-03-24 20:20:54 -0300

    Primeiro Deploy

---
+++ vetores/Program.vb
@@ -0,0 +1,11 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+     dim nomecompleto(2) as String
+       nomecompleto(0) = "Ricardo"
+       nomecompleto(1) = "Mendes"
+
+       console.Writeline(nomecompleto(0) & "" & " " & nomecompleto(1))
+    End Sub
+End Module
