7e44cde357b9b00c998ebab604ec69b306ba8bb0
ricksilva2 <silvarico474@gmail.com>
2026-03-24 20:52:46 -0300

    segundo Deploy

---
+++ sistemas/Program.vb
@@ -0,0 +1,14 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+    Dim os(3) As String
+     os(0) = "Linux"
+     os(1) = "Windows"
+     os(2) = "IOS"
+
+     Console.WriteLine(os(0))
+     Console.WriteLine(os(1))
+     Console.WriteLine(os(2))
+     End Sub
+End Module
